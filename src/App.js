b26595ddab0987683b2cf33dac1191c55911ddae
firmanasgani <firmanasgani37@gmail.com>
2024-01-09 19:41:28 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from './components//Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [value, setValue] = useState('')
+    const [todos, setTodos] = useState([
+      {title: 'Susu Ultra', count: 1},
+      {title: 'Tahu Sumedang', count: 1},
+      {title: 'Semangka', count: 1}
+    ])
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+
+      if(!value) {
+        alert ('No Blank List')
+        return
+      }
+
+      const addedTodos = [...todos, {
+        title: value,
+        count: 1
+      }]
+
+      setTodos(addedTodos)
+      setValue('')
+    }
+
+    const handleAdditionCount = (index) => {
+      const newTodos = [...todos]
+
+      newTodos[index].count = newTodos[index].count + 1
+      setTodos(newTodos)
+
+    }
+
+    const handleSubstractionCount = (index) => {
+      const newTodos = [...todos]
+      const indexedCountProperty = newTodos[index].count 
+
+      if (indexedCountProperty > 0 ) {
+        newTodos[index].count = newTodos[index].count - 1
+      } else {
+        newTodos.splice(index, 1)
+      }
+      setTodos(newTodos)
+    }
+
+    const getTotalCounts = () => {
+      const totalCounts = todos.reduce((total, num) => {
+        return total + num.count
+      }, 0)
+
+      return totalCounts
+    }
+
+    return (
+      <>
+        <Navbar />
+      </>
+    );
+ 
+
 }
 
-export default App;
+export default App
